afc386bf1a1572c3d2166d2b4eb1626137415c31
vitor <enzovitor598@gmail.com>
2026-04-17 14:23:36 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,62 @@
+let listaAlunos = [];
+
+document.getElementById("btnCadastrar").addEventListener("click", cadastrarNota);
+
+function cadastrarNota() {
+
+    let nome = document.getElementById("Nome").value; 
+    let n1 = document.getElementById("nota1").value;
+    let n2 = document.getElementById("nota2").value;
+
+    
+    if (nome === "" || n1 === "" || n2 === "") {
+        alert("Preencha todos os campos corretamente!");
+        return;
+    }
+
+    let nota1 = Number(n1);
+    let nota2 = Number(n2);
+    let media = (nota1 + nota2) / 2;
+
+    let situacao = media >= 7 ? "Aprovado" : "Reprovado";
+
+    let novoAluno = {
+        nome: nome, 
+        n1: nota1,
+        n2: nota2,
+        media: media, 
+        situacao: situacao
+    };
+
+    listaAlunos.push(novoAluno);
+    exibir();
+    limparCampos();
+}
+
+function exibir() {
+    let divMedia = document.getElementById("media");
+
+    let conteudoHTML = listaAlunos.map(aluno => {
+        let corStatus = aluno.situacao === "Aprovado" ? "green" : "red";
+
+        return `
+            <p>
+                <strong>Aluno:</strong> ${aluno.nome} | 
+                <strong>Média:</strong> ${aluno.media} | 
+                <span style="color: ${corStatus}">
+                    <strong>Status:</strong> ${aluno.situacao}
+                </span>
+            </p>
+            <hr>
+        `;
+    }).join("");
+
+    divMedia.innerHTML = conteudoHTML;
+}
+
+function limparCampos() {
+    document.getElementById("Nome").value = "";
+    document.getElementById("nota1").value = "";
+    document.getElementById("nota2").value = "";
+    document.getElementById("Nome").focus(); 
+}
